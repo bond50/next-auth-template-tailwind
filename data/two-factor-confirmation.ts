6a2bdab621bd3474c8920c7b4f5c1acd3d140eeb
bobd50 <galavu10@gmail.com>
2025-07-22 15:21:12 +0300

    added 2fa data for confirmation

---
+++ data/two-factor-confirmation.ts
@@ -0,0 +1,16 @@
+import {db} from "@/lib/db";
+
+export const getTwoFactorConfirmationByUserId = async (userId: string) => {
+    try {
+        return await db.twoFactorConfirmation.findUnique({
+            where: {
+                userId
+            }
+        });
+    } catch {
+        return null;
+    }
+
+}
+
+
