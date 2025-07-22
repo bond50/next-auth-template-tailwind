cd5e928fbd286dae1029fdc03d44a4cc14a78f57
bobd50 <galavu10@gmail.com>
2025-07-22 19:00:24 +0300

    creating userinfo component

--- app/(protected)/server/page.tsx
+++ app/(protected)/server/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {auth} from "@/auth";
+import {currentUser} from "@/lib/auth";
 
 const ServerPage = async () => {
-    const session = await auth()
+   const user = await currentUser()
     return (
         <div>
+            {JSON.stringify(user, null, 2)}
             Server page
         </div>
     );
